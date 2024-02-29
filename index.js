472f8edf2b9fe3dd0bf3006f92babaaa0828645c
Jay Sharma <102017466+jaysharma2000@users.noreply.github.com>
2024-02-29 20:49:30 +0530

    Changes Done..

---
+++ index.js
@@ -0,0 +1,92 @@
+document.getElementsByTagName("button")[1].addEventListener('click', function(){
+    document.getElementsByTagName("img")[0].src="./nature/bg11.jpg";
+    document.getElementsByTagName("img")[1].src="./nature/bg2.jpg";
+    document.getElementsByTagName("img")[2].src="./nature/bg3.jpg";
+    document.getElementsByTagName("img")[3].src="./nature/bg4.jpg";
+    document.getElementsByTagName("img")[4].src="./nature/bg5.jpg";
+    document.getElementsByTagName("img")[5].src="./nature/bg6.jpg";
+    
+    document.getElementsByTagName("img")[6].classList.remove("card");
+    document.getElementsByTagName("img")[6].style.display = "none";
+    document.getElementsByTagName("img")[7].classList.remove("card");
+    document.getElementsByTagName("img")[7].style.display = "none";
+    document.getElementsByTagName("img")[8].classList.remove("card");
+    document.getElementsByTagName("img")[8].style.display = "none";
+    document.getElementsByTagName("img")[9].classList.remove("card");
+    document.getElementsByTagName("img")[9].style.display = "none";
+    document.getElementsByTagName("img")[10].classList.remove("card");
+    document.getElementsByTagName("img")[10].style.display = "none";
+    document.getElementsByTagName("img")[11].classList.remove("card");
+    document.getElementsByTagName("img")[11].style.display = "none";
+});
+
+document.getElementsByTagName("button")[0].addEventListener('click', function(){
+    document.getElementsByTagName("img")[0].src="https://codehelp-image-gallery.netlify.app/images/image1.jpg";
+    document.getElementsByTagName("img")[1].src="https://codehelp-image-gallery.netlify.app/images/image2.jpg";
+    document.getElementsByTagName("img")[2].src="https://codehelp-image-gallery.netlify.app/images/image3.jpg";
+    document.getElementsByTagName("img")[3].src="https://codehelp-image-gallery.netlify.app/images/image4.jpg";
+    document.getElementsByTagName("img")[4].src="https://codehelp-image-gallery.netlify.app/images/image5.jpg";
+    document.getElementsByTagName("img")[5].src="https://codehelp-image-gallery.netlify.app/images/image6.jpg";
+    
+    document.getElementsByTagName("img")[6].style.display = "block";
+    
+    document.getElementsByTagName("img")[7].style.display = "block";
+    
+    document.getElementsByTagName("img")[8].style.display = "block";
+    
+    document.getElementsByTagName("img")[9].style.display = "block";
+
+    document.getElementsByTagName("img")[10].style.display = "block";
+
+    document.getElementsByTagName("img")[11].style.display = "block";    
+});
+
+
+document.getElementsByTagName("button")[2].addEventListener('click', function(){
+    document.getElementsByTagName("img")[0].src="./animals/a1.jpg";
+    document.getElementsByTagName("img")[1].src="./animals/a2.jpg";
+    document.getElementsByTagName("img")[2].src="./animals/a3.jpg";
+    document.getElementsByTagName("img")[3].src="./animals/a4.jpg";
+    document.getElementsByTagName("img")[4].src="./animals/a5.jpg";
+    document.getElementsByTagName("img")[5].src="./animals/a6.jpg";
+    
+    document.getElementsByTagName("img")[6].classList.remove("card");
+    document.getElementsByTagName("img")[6].style.display = "none";
+    document.getElementsByTagName("img")[7].classList.remove("card");
+    document.getElementsByTagName("img")[7].style.display = "none";
+    document.getElementsByTagName("img")[8].classList.remove("card");
+    document.getElementsByTagName("img")[8].style.display = "none";
+    document.getElementsByTagName("img")[9].classList.remove("card");
+    document.getElementsByTagName("img")[9].style.display = "none";
+    document.getElementsByTagName("img")[10].classList.remove("card");
+    document.getElementsByTagName("img")[10].style.display = "none";
+    document.getElementsByTagName("img")[11].classList.remove("card");
+    document.getElementsByTagName("img")[11].style.display = "none";
+});
+
+document.getElementsByTagName("button")[3].addEventListener('click', function(){
+    document.getElementsByTagName("img")[0].src="./flowers/f1.jpg";
+    document.getElementsByTagName("img")[1].src="./flowers/f2.jpg";
+    document.getElementsByTagName("img")[2].src="./flowers/f3.jpg";
+    document.getElementsByTagName("img")[3].src="./flowers/f4.jpg";
+    document.getElementsByTagName("img")[4].src="./flowers/f5.jpg";
+    document.getElementsByTagName("img")[5].src="./flowers/f6.jpg";
+    
+    document.getElementsByTagName("img")[6].classList.remove("card");
+    document.getElementsByTagName("img")[6].style.display = "none";
+    document.getElementsByTagName("img")[7].classList.remove("card");
+    document.getElementsByTagName("img")[7].style.display = "none";
+    document.getElementsByTagName("img")[8].classList.remove("card");
+    document.getElementsByTagName("img")[8].style.display = "none";
+    document.getElementsByTagName("img")[9].classList.remove("card");
+    document.getElementsByTagName("img")[9].style.display = "none";
+    document.getElementsByTagName("img")[10].classList.remove("card");
+    document.getElementsByTagName("img")[10].style.display = "none";
+    document.getElementsByTagName("img")[11].classList.remove("card");
+    document.getElementsByTagName("img")[11].style.display = "none";
+});
+
+
+
+
+
